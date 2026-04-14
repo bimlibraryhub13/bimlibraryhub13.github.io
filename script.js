8fd86cf34d471db8a40fc9451c8d28ebabd98120
bimlibraryhub13 <bimlibraryhub13@gmail.com>
2026-04-14 10:03:33 +0530

    File uploda

---
+++ script.js
@@ -0,0 +1,4 @@
+
+// ======================= script.js =======================
+function toggleMenu(){document.getElementById('menu').classList.toggle('active')}
+function scrollToSection(id){document.getElementById(id).scrollIntoView({behavior:'smooth'})}
